1bfd6be7370eecc1759289b74fbb6a7d1672fe9c
Elias <ejais58@gmail.com>
2022-05-29 17:01:46 -0300

    loadchildren correccion

--- Front-Login/src/app/app-routing.module.ts
+++ Front-Login/src/app/app-routing.module.ts
@@ -7,17 +7,14 @@ import { LoginComponent } from './modules/login/login.component';
 const routes: Routes = [
   {
     path:'',
-    component: LoginComponent,
     loadChildren:() => import(`@modules/login/login.module`).then(m => m.LoginModule)
   },
   {
     path: 'register',
-    component: RegisterComponent,
     loadChildren:() => import(`@modules/register/register.module`).then(m => m.RegisterModule)
   },
   {
     path: 'home',
-    component: HomeComponent,
     loadChildren:() => import(`@modules/home/home.module`).then(m => m.HomeModule)
   }
 ];
--- Front-Login/src/app/modules/register/register-routing.module.ts
+++ Front-Login/src/app/modules/register/register-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
